c94ab4ac9f9a1f97822a0e420364aeaebdf723cd
Cristian <cristianguzmansuarez@gmail.com>
2019-06-03 22:04:47 -0500

    Version inicial del portfolio

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(function(){
+    $("#social").load("templates/social.html");
+    $("#header").load("templates/header.html");
+    $("#menu").load("templates/menu.html");
+    $("#footer").load("templates/footer.html");
+});
